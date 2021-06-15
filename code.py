1fdc585ba442c4e71b54d8a2b06e6ef43fe68a8b
Ridhi Sharda <60606087+ridhisharda@users.noreply.github.com>
2021-06-15 11:51:30 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,10 @@
+import plotly.figure_factory as ff
+import statistics 
+import random
+import pandas as pd
+import csv
+df= pd.read_csv("data.csv")
+data= df["temp"].tolist()
+population_mean= statistics.mean(data)
+fig= ff.create_distplot([data], ["temp"], show_hist=False)
+fig.show()
